203ac16f60b6a26b4f9cb8e31691b87a31580949
imaydar <planeta_aydar@mail.ru>
2021-02-08 21:12:25 +0500

    added reconnect feature

--- Esp/DHT22WebServer/DHT22WebServer/DHT22WebServer.ino
+++ Esp/DHT22WebServer/DHT22WebServer/DHT22WebServer.ino
@@ -1,166 +1,77 @@
-/*********
-  Руи Сантос (Rui Santos)
-  Более подробно о проекте на: http://randomnerdtutorials.com  
-*********/
-
-// подключаем библиотеку «ESP8266WiFi»:
 #include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
 #include "DHT.h"
 
-// оставляем раскомментированной строчку, 
-// соответствующую модели вашего датчика:
-//#define DHTTYPE DHT11     // DHT 11
-//#define DHTTYPE DHT21   // DHT 21 (AM2301)
-#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321  float h = dht.readHumidity() считываем температуру в Цельсиях (по умолчанию): float t = dht.readTemperature();
 
-// вписываем здесь данные для своей WiFi-сети:
-const char* ssid = "TakieDela";
-const char* password = "20978686";
-
-// веб-сервер на порте 80:
-WiFiServer server(80);
-
-// датчик DHT:
+#define DHTTYPE DHT22 
 const int DHTPin = 4;
-// инициализируем датчик DHT:
 DHT dht(DHTPin, DHTTYPE);
 
-// временные переменные:
-static char celsiusTemp[7];
-static char fahrenheitTemp[7];
-static char humidityTemp[7];
+const char* ssid = "TakieDela";
+const char* password = "20978686";
+IPAddress ip (192,168,0,178);
+IPAddress dns (8, 8, 8, 8);
+IPAddress gateway (192, 168, 0, 1);
+IPAddress mask (255, 255, 255, 0);
 
-// этот блок будет запускаться только при загрузке ESP:
-void setup() {
-  // инициализируем последовательный порт (в отладочных целях):
-  Serial.begin(115200);
-  delay(10);
+ESP8266WebServer server(80);
 
-  dht.begin();
-  
-  // подключаемся к WiFi-сети:
-  Serial.println();
-  Serial.print("Connecting to ");  //  "Подключаемся к "
-  Serial.println(ssid); 
-  
-  WiFi.begin(ssid, password);
-  
+void connectWifi(){
+  int status = WL_IDLE_STATUS;
+  //WiFi.config(ip, gateway, mask, dns, dns);
+  // attempt to connect to Wifi network:
+    WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:    
+    status = WiFi.begin(ssid, password);
+    // wait 10 seconds for connection:
+    delay(10000);
   }
-  Serial.println("");
-  Serial.println("WiFi connected");  
-             //  "Подключение к WiFi выполнено"
-  
-  // запускаем веб-сервер:
-  server.begin();
-  Serial.println("Web server running. Waiting for the ESP IP...");
-              // "Веб-сервер запущен. Ждем IP-адрес ESP..."
-  delay(10000);
-  
-  // печатаем IP-адрес ESP:
+  // print your WiFi shield's IP address:
+  Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
 }
 
-// этот блок будет запускаться снова и снова:
-void loop() {
-  // начинаем прослушку новых клиентов:
-  WiFiClient client = server.available();
-  
-  if (client) {
-    Serial.println("New client");  //  "Новый клиент"
-    // создаем переменную типа «boolean»,
-    // чтобы определить конец HTTP-запроса:
-    boolean blank_line = true;
-    while (client.connected()) {
-      if (client.available()) {
-        char c = client.read();
-        
-        if (c == '\n' && blank_line) {
-            // данные от датчика могут запаздывать на 2 секунды
-            // (это очень медленный датчик):
-           
-            // считываем температуру в Фаренгейтах
-            // (isFahrenheit = true):
-            float f = dht.readTemperature(true);
-            // проверяем, прочитались ли данные от датчика, 
-            // и если нет, то выходим и начинаем заново:
-            if (isnan(h) || isnan(t) || isnan(f)) {
-              Serial.println("Failed to read from DHT sensor!");
-                          // "Не удалось прочитать 
-                          // данные от датчика DHT"
-              strcpy(celsiusTemp,"Failed");
-              strcpy(fahrenheitTemp, "Failed");
-              strcpy(humidityTemp, "Failed");         
-            }
-            else{
-              // рассчитываем градусы в Цельсиях и Фаренгейтах,
-              // а также влажность:
-              float hic = dht.computeHeatIndex(t, h, false);       
-              dtostrf(hic, 6, 2, celsiusTemp);             
-              float hif = dht.computeHeatIndex(f, h);
-              dtostrf(hif, 6, 2, fahrenheitTemp);         
-              dtostrf(h, 6, 2, humidityTemp);
+void handleRoot() {
+  // данные от датчика могут запаздывать на 2 секунды
+  // (это очень медленный датчик):
+  float h = dht.readHumidity();
+  // считываем температуру в Цельсиях (по умолчанию):
+  float t = dht.readTemperature(); 
+  server.send(200, "text/plain", "temp is " + String(t) + " humidity is " + String(h));
+}
 
-              // все эти Serial.print() ниже можно удалить,
-              // т.к. они для отладочных целей:
-              Serial.print("Humidity: ");  //  "Влажность: "
-              Serial.print(h);
-              Serial.print(" %\t Temperature: ");  //  "Температура: "
-              Serial.print(t);
-              Serial.print(" *C ");
-              Serial.print(f);
-              Serial.print(" *F\t Heat index: ");
-                             //  "Тепловой индекс: "
-              Serial.print(hic);
-              Serial.print(" *C ");
-              Serial.print(hif);
-              Serial.print(" *F");
-              Serial.print("Humidity: ");  //  "Влажность: "
-              Serial.print(h);
-              Serial.print(" %\t Temperature: ");  //  "Температура: "
-              Serial.print(t);
-              Serial.print(" *C ");
-              Serial.print(f);
-              Serial.print(" *F\t Heat index: ");
-                             //  "Тепловой индекс: "
-              Serial.print(hic);
-              Serial.print(" *C ");
-              Serial.print(hif);
-              Serial.println(" *F"); 
-            }
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-Type: text/html");
-            client.println("Connection: close");
-            client.println();
-            // веб-страница, отображающая температуру и влажность:
-            client.println("<!DOCTYPE HTML>");
-            client.println("<html>");
-            client.println("<head></head><body><h1>ESP8266 - Temperature and Humidity</h1><h3>Temperature in Celsius: ");
-            client.println(celsiusTemp);
-            client.println("*C</h3><h3>Temperature in Fahrenheit: ");
-            client.println(fahrenheitTemp);
-            client.println("*F</h3><h3>Humidity: ");
-            client.println(humidityTemp);
-            client.println("%</h3><h3>");
-            client.println("</body></html>");     
-            break;
-        }
-        if (c == '\n') {
-          // если обнаружен переход на новую строку:
-          blank_line = true;
-        }
-        else if (c != '\r') {
-          // если в текущей строчке найден символ: 
-          blank_line = false;
-        }
-      }
-    }  
-    // закрываем соединение с клиентом:
-    delay(1);
-    client.stop();
-    Serial.println("Client disconnected.");
-               //  "Клиент отключен."
-  }
+void handleTemp(){
+  float t = dht.readTemperature(); 
+  server.send(200, "text/plain", String(t));
+}
+
+void handleHumidity(){
+  float h = dht.readHumidity();
+  server.send(200, "text/plain", String(h));
+}
+
+void setUpServer(){
+  server.on("/", handleRoot);
+  server.on("/temp", handleTemp);
+  server.on("/humidity", handleHumidity);
+  server.begin();
+  Serial.println("HTTP server started");
+}
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  connectWifi();
+  setUpServer();
+  dht.begin();
+}
+
+void loop() {
+  if((WiFi.status() != WL_CONNECTED))
+    connectWifi();
+  server.handleClient();
 }
--- Esp/Roll_A4988_Nema17/Roll_A4988_Nema17.ino
+++ Esp/Roll_A4988_Nema17/Roll_A4988_Nema17.ino
@@ -6,17 +6,24 @@ int dir  = 2; //GPIO2---D4 of Nodemcu--Direction of stepper motor driver
 int enable = 4;//GPIO2---D2 enable pin
 int maxSteps = 600;
 int minSteps = 1;
-int delaySteps = 2;//2 is max speed
-//int _step = 1; // rotate direction.
+int delaySteps = 5;//2 is max speed, 3 is not enough when it's windy
 bool rotating = false;
-String steps = "50";
 
+void initializeDriver(){
+  pinMode(enable, OUTPUT);
+  pinMode(step, OUTPUT); //Step pin as output
+  pinMode(dir,  OUTPUT); //Direcction pin as output
+  digitalWrite(step, LOW); // Currently no stepper motor movement
+  digitalWrite(dir, LOW);  
+  disableDriver();
+}
 void go(int steps, int inputDirection){
     if(steps > maxSteps) steps = maxSteps;
     if(steps < minSteps) steps = minSteps;
 
     digitalWrite(dir, inputDirection);//set direction
     enableDriver();//to prevent both motor and driver from overheat disable it after done
+    rotating = true;
     for(int i = 0; i < steps; i++){
       digitalWrite(step, HIGH);
       delay(delaySteps);
@@ -24,6 +31,7 @@ void go(int steps, int inputDirection){
       delay(delaySteps);
     }
     disableDriver();
+    rotating = false;
 }
 void enableDriver(){
   digitalWrite(enable, LOW);
@@ -40,122 +48,76 @@ void disableDriver(){
 #include <ESP8266mDNS.h>
 
 const char* ssid = "TakieDela";
-const char* password = "YourPass";
+const char* password = "20978686";
+IPAddress ip (192,168,0,177);
+IPAddress dns (8, 8, 8, 8);
+IPAddress gateway (192, 168, 0, 1);
+IPAddress mask (255, 255, 255, 0);
 
 ESP8266WebServer server(80);
 
+String ind = "<!DOCTYPE html><html><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=windows-1252\"><title>ESP Web Server</title><meta name=\"viewport\" content=\"width=device-width,initial-scale=1\"><style>html{font-family:Arial;display:inline-block;text-align:center}h2{font-size:2.6rem}body{max-width:600px;margin:0 auto;padding-bottom:10px}button{border-radius:50%;width:70px;height:70px;background-color:#fff}.steps{width:100px;font-size:50px;margin-left:20px;text-align:right}</style></head><body><h2>Automated window</h2><p>Steps<input type=\"number\" value=\"50\" min=\"1\" max=\"600\" class=\"steps\" onkeypress=\"return event.charCode &gt;= 48 &amp;&amp; event.charCode &lt;= 57\"></p><div><button onclick=\"rotate(0)\">Open</button> <button onclick=\"rotate(1)\">Close</button></div><button onclick=\"hold()\">Hold</button><script>function rotate(t){var e=new XMLHttpRequest;e.open(\"GET\",\"/update?direction=\"+t+\"&steps=\"+document.getElementsByClassName(\"steps\")[0].value,!1),e.send()}function hold(){var t=new XMLHttpRequest;t.open(\"GET\",\"/hold\",!1),t.send()}</script></body></html>";
 
-
-/*const char index_html[] PROGMEM = R"rawliteral(*/
-String ind = "<!DOCTYPE HTML><html>"
-"<head>"
-"  <title>ESP Web Server</title>"
-"  <meta name='viewport' content='width=device-width, initial-scale=1'>"
-"  <style>"
-"    html {font-family: Arial; display: inline-block; text-align: center;}"
-"    h2 {font-size: 2.6rem;}"
-"    body {max-width: 600px; margin:0px auto; padding-bottom: 10px;}"
-"    .switch {position: relative; display: inline-block; width: 120px; height: 68px} "
-"    .switch input {display: none}"
-"    .slider {position: absolute; top: 0; left: 0; right: 0; bottom: 0; background-color: #ccc; border-radius: 34px}"
-"    .slider:before {position: absolute; content: ''; height: 52px; width: 52px; left: 8px; bottom: 8px; background-color: #fff; -webkit-transition: .4s; transition: .4s; border-radius: 68px}"
-"    input:checked+.slider {background-color: #2196F3}"
-"    input:checked+.slider:before {-webkit-transform: translateX(52px); -ms-transform: translateX(52px); transform: translateX(52px)}"
-"  </style>"
-"</head>"
-"<body>"
-"  <h2>ESP Web Server</h2>"
-"  <button onclick='logoutButton()'>Logout</button>"
-"<p><label class='switch'><input type='checkbox' onchange='toggleCheckbox(this)' id='output'><span class='slider'></span></label></p><input value='50' class='steps' type='text' onkeypress='return event.charCode >= 48 &amp;&amp; event.charCode <= 57' style='width: 300px;font-size: 50px;'>"
-"<script>function toggleCheckbox(element) {"
-"  var xhr = new XMLHttpRequest();"
-"  if(element.checked){ xhr.open(\"GET\", \"/update?direction=1&steps=\" + document.getElementsByClassName(\"steps\")[0].value, false)}"
-"  else { xhr.open(\"GET\", \"/update?direction=0&steps=\" + document.getElementsByClassName(\"steps\")[0].value, false); }"
-"  xhr.send();"
-"}"
-"function logoutButton() {"
-"  var xhr = new XMLHttpRequest();"
-"  xhr.open(\"GET\", \"/logout\", true);"
-"  xhr.send();"
-"  setTimeout(function(){ window.open(\"/logged-out\",\"_self\"); }, 1000);"
-"}"
-""
-"</script>"
-"</body>"
-"</html>";
-
-// Replaces placeholder with button section in your web page
-String processor(const String& var){
-  //Serial.println(var);
-  if(var == "BUTTONPLACEHOLDER"){
-    String buttons ="";
-    buttons+= "<p><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"output\"><span class=\"slider\"></span></label></p><input value=\"50\" class=\"steps\" type=\"text\" onkeypress=\"return event.charCode >= 48 &amp;&amp; event.charCode <= 57\" style=\"width: 300px;font-size: 50px;\">";
-    return buttons;
-  }
-  return String();
-}
-
-
-void handleRoot() {
-  server.send(200, "text/plain", "hello from esp8266!");
-}
-
-
-void setup(void) {
-  Serial.begin(115200);
-
-  pinMode(enable, OUTPUT);
-  pinMode(step, OUTPUT); //Step pin as output
-  pinMode(dir,  OUTPUT); //Direcction pin as output
-  digitalWrite(step, LOW); // Currently no stepper motor movement
-  digitalWrite(dir, LOW);  
-  disableDriver();
-  
-  WiFi.mode(WIFI_STA);
+void connectWifi(){
+  int status = WL_IDLE_STATUS;
+  WiFi.config(ip, gateway, mask, dns, dns);
+  // attempt to connect to Wifi network:
   WiFi.begin(ssid, password);
-  Serial.println("");
-
-  // Wait for connection
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:    
+    WiFi.begin(ssid, password);
+    // wait 10 seconds for connection:
+    delay(10000);
   }
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
+
+  // print your WiFi shield's IP address:
+  Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
 
   if (MDNS.begin("esp8266")) {
     Serial.println("MDNS responder started");
   }
-
-  server.on("/",  [](){
+}
+void setUpServer(){
+  server.on("/",  [] (){
     server.send(200, "text/html",ind );
   });
-
-   server.on("/update",  [] () {
-    // GET input1 value on <ESP_IP>/update?state=<inputMessage>
-    
-    if (server.arg("direction") != "") {
-      String inputDirection = server.arg("direction");
-      String inputSteps = server.arg("steps");
-      Serial.println("direction - " + inputDirection + " steps - " + inputSteps);
-      //_step = inputDirection.toInt();
-      int count = inputSteps.toInt();
-      rotating = true;
-      
-      //digitalWrite(dir, _step); //Rotate stepper motor in clock wise direction
-      go(count, inputDirection.toInt());
-    }
-    server.send(200, "text/plain", "OK");     
+  
+  server.on("/hold", [] (){
+    enableDriver();
+    server.send(200, "text/plain", "OK");    
   });
+
+  server.on("/update", handleUpdate);
   
   server.begin();
   Serial.println("HTTP server started");
 }
 
+void handleUpdate() {
+  if (server.arg("direction") != "") {
+      String inputDirection = server.arg("direction");
+      String inputSteps = server.arg("steps");
+      Serial.println("direction - " + inputDirection + " steps - " + inputSteps);
+      int count = inputSteps.toInt();  
+      go(count, inputDirection.toInt());
+    }
+    server.send(200, "text/plain", "OK");     
+}
+
+
+void setup(void) {
+  Serial.begin(115200);
+  initializeDriver();
+  connectWifi();
+  setUpServer();
+}
+
 void loop(void) {
+  if(WiFi.status() != WL_CONNECTED)
+    connectWifi();
   server.handleClient();
   MDNS.update();
 }
